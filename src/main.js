10bcaa0ab7a7c4f3a6e48a386071839baa58ebb6
Larysa <draglara@ukr.net>
2025-01-23 19:44:42 +0200

    update is

---
+++ src/main.js
@@ -0,0 +1,104 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { fetchPhotos } from './js/pixabay-api';
+import { renderPhotoCards } from './js/render-functions';
+
+const formEl = document.querySelector('.form-search');
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.load-more-btn');
+
+let page = 1;
+let query = '';
+
+loader.style.display = 'none';
+
+const onFormSubmit = async event => {
+  try {
+    event.preventDefault();
+    galleryContainer.innerHTML = '';
+    loader.style.display = 'block';
+
+    const query = event.currentTarget.elements.user_query.value.trim();
+
+    if (query === '') {
+      iziToast.warning({
+        title: 'Warning',
+        message: 'Please enter a search query!',
+      });
+      return;
+    }
+
+    page = 1;
+    loadMoreBtn.classList.add('is-hidden');
+
+    const { data } = await fetchPhotos(query, page);
+
+    loader.style.display = 'none';
+
+    if (!data.hits.length) {
+      iziToast.error({
+        title: 'Error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+      return;
+    }
+    const markup = renderPhotoCards(data.hits);
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+    const lightbox = new SimpleLightbox('.gallery-item', {
+      captions: true,
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+    lightbox.refresh();
+
+    formEl.reset();
+
+    if (data.totalHits > 1) {
+      loadMoreBtn.classList.remove('is-hidden');
+
+      loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+    }
+  } catch (error) {
+    loader.style.display = 'none';
+    // iziToast.error({
+    //   title: 'Error',
+    //   message: 'Failed to load images. Please try again later.',
+    // });
+  }
+};
+
+formEl.addEventListener('submit', onFormSubmit);
+
+const onLoadMoreBtnClick = async event => {
+  try {
+    page++;
+
+    const { data } = await fetchPhotos(query, page);
+
+    const markup = renderPhotoCards(data.hits);
+    galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+    if (page === data.totalHits) {
+      loadMoreBtn.classList.add('is-hidden');
+      loadMoreBtn.removeEventListener('click', onLoadMoreBtnClick);
+
+      iziToast.info({
+        title: 'Info',
+        message: "We're sorry, but you've reached the end of search results.",
+      });
+      return;
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to load images. Please try again later.',
+    });
+  }
+};
